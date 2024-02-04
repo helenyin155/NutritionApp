6788b4b3142cd748636b5f470d4c96d9e143c2d4
fehervaryy <martindmhe@gmail.com>
2024-02-04 18:47:34 -0500

    Co-authored-by: helenyin155 <helenyin155@users.noreply.github.com>

--- flutterapp/lib/pages/home_page.dart
+++ flutterapp/lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import "package:flutter/material.dart";
+import "package:flutterapp/pages/body_page.dart";
+import "package:flutterapp/pages/camera_page.dart";
+import "package:flutterapp/pages/profile_page.dart";
+import "package:flutterapp/pages/tracker_page.dart";
 
 int hexToInteger(String hex) => int.parse(hex, radix: 16);
 
@@ -13,20 +17,32 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-void handleLogOut() {
-
-}
-
 class _HomePageState extends State<HomePage> {
+  final _pageList = [
+    BodyHome(),
+    TrackerPage(),
+    CameraPage(),
+    ProfilePage(),
+  ];
+
+  void handleLogOut() {}
+
+  int _selectedIndex = 0;
+
+  void _handleNavigation(int index) {
+    if (index != _selectedIndex) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(hexToInteger('FFF5F3ED')),
       appBar: AppBar(
-        leading: Icon(
-          Icons.account_circle,
-          size: 40.0,
-          color: Colors.black),
+        leading: Icon(Icons.account_circle, size: 40.0, color: Colors.black),
         title: Text("Logo Placeholder"),
         actions: [
           Padding(
@@ -41,7 +57,23 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
-      body: Center()
+      body: Center(child: _pageList[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _handleNavigation,
+        iconSize: 32.0,
+        fixedColor: Colors.black,
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.receipt), label: 'Tracker'),
+          BottomNavigationBarItem(icon: Icon(Icons.document_scanner), label: 'Camera'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          
+        ],
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+      ),
     );
   }
 }
