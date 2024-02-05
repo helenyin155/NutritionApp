43689a894c1092040430aaa820c8c06a07f85f8a
fehervaryy <martindmhe@gmail.com>
2024-02-04 19:55:25 -0500

    disabled ripple animation on navigation

--- flutterapp/lib/pages/home_page.dart
+++ flutterapp/lib/pages/home_page.dart
@@ -40,40 +40,47 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(hexToInteger('FFF5F3ED')),
-      appBar: AppBar(
-        leading: Icon(Icons.account_circle, size: 40.0, color: Colors.black),
-        title: Text("Logo Placeholder"),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-              onTap: handleLogOut,
-              child: Icon(
-                Icons.logout,
-                size: 32.0,
+        backgroundColor: Color(hexToInteger('FFF5F3ED')),
+        appBar: AppBar(
+          leading: Icon(Icons.account_circle, size: 40.0, color: Colors.black),
+          title: Text("Logo Placeholder"),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                onTap: handleLogOut,
+                child: Icon(
+                  Icons.logout,
+                  size: 32.0,
+                ),
               ),
-            ),
-          )
-        ],
-      ),
-      body: Center(child: _pageList[_selectedIndex]),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: _handleNavigation,
-        iconSize: 32.0,
-        fixedColor: Colors.black,
-        currentIndex: _selectedIndex,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.receipt), label: 'Tracker'),
-          BottomNavigationBarItem(icon: Icon(Icons.document_scanner), label: 'Camera'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-          
-        ],
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-      ),
-    );
+            )
+          ],
+        ),
+        body: Center(child: _pageList[_selectedIndex]),
+        bottomNavigationBar: Theme(
+          data: ThemeData(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+          ),
+          child: BottomNavigationBar(
+            onTap: _handleNavigation,
+            iconSize: 32.0,
+            fixedColor: Colors.black,
+            currentIndex: _selectedIndex,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.receipt), label: 'Tracker'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.document_scanner), label: 'Camera'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person), label: 'Profile'),
+            ],
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+          ),
+        ));
   }
 }
